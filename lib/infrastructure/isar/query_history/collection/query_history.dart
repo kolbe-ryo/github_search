89575de809b46aef6cb8fa4a051b3df7368f807f
kolbe <water.northground@gmail.com>
2023-07-09 18:26:36 +0900

    isar for inflastracture

---
+++ lib/infrastructure/isar/query_history/collection/query_history.dart
@@ -0,0 +1,19 @@
+import 'package:isar/isar.dart';
+
+part 'query_history.g.dart';
+
+/// SQLで言うテーブルのこと
+@Collection()
+class QueryHistoryCollection {
+  Id id = Isar.autoIncrement;
+
+  /// 検索文字列
+  late String queryString;
+
+  /// 検索日時
+  @Index()
+  late DateTime searchedAt;
+
+  @override
+  String toString() => '{id: $id, queryString: $queryString, searchedAt: $searchedAt}';
+}
