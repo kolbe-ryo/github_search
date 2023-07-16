b00e35f716a2939d66576bff5273725fa91bc9c1
kolbe <water.northground@gmail.com>
2023-07-16 16:54:23 +0900

    extension for processing async value data error response

---
+++ lib/util/extention.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+extension AsyncValueExt<T> on AsyncValue<T> {
+  // エラーのみ処理する
+  void whenError(
+    void Function(Object error, StackTrace? stackTrace) error,
+  ) {
+    if (isLoading) {
+      return;
+    }
+
+    return map<void>(
+      data: (_) {},
+      error: (e) => error(e.error, e.stackTrace),
+      loading: (_) {},
+    );
+  }
+}
