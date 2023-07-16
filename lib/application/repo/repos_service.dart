766c1635a09e2d0241eed75742ab3a877030d11d
kolbe <water.northground@gmail.com>
2023-07-16 15:38:25 +0900

    cretate repos service for application

---
+++ lib/application/repo/repos_service.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'state/repos_state.dart';
+
+final reposServiceProvider = Provider((ref) => ReposService.new);
+
+/// リポジトリ一覧サービス
+class ReposService {
+  const ReposService(this.ref);
+
+  final Ref ref;
+
+  /// 次のページを取得する
+  Future<void> fetchNextPage() async {
+    await ref.read(reposStateProvider.notifier).fetchNextPage();
+  }
+}
