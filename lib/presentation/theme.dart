8901e07cb642f1790691ce8389d5f4bfef937e92
kolbe <water.northground@gmail.com>
2023-07-16 16:27:32 +0900

    add theme provider

---
+++ lib/presentation/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final themeProvider = Provider.family<ThemeData, Brightness>(
+  (ref, brightness) {
+    return ThemeData(
+      colorScheme: ColorScheme.fromSwatch(
+        brightness: brightness,
+        primarySwatch: Colors.grey,
+      ),
+      // fontFamily: FontFamily.murecho,
+      bottomSheetTheme: const BottomSheetThemeData(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+          ),
+        ),
+      ),
+    );
+  },
+);
