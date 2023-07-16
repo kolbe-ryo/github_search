2a28f38b558b6bc4fdff988fc89feee9f31bbe86
kolbe <water.northground@gmail.com>
2023-07-16 16:19:08 +0900

    search_repo_searvice implement

--- lib/application/repo/state/query_histories.dart
+++ lib/application/repo/state/query_histories.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../domain/repositories/query_history/entity/query_history.dart';
-import '../../../infrastructure/isar/query_history/query_history_repository.dart';
+import '../../../domain/repositories/query_history/query_history_repository.dart';
 import 'search_repos_query.dart';
 
 /// リポジトリ検索履歴一覧プロバイダー
--- lib/infrastructure/isar/query_history/query_history_repository.dart
+++ lib/infrastructure/isar/query_history/query_history_repository.dart
@@ -1,51 +1,104 @@
-// Package imports:
+// Copyright 2022 susatthi All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:isar/isar.dart';
 
-// Project imports:
 import '../../../domain/repositories/query_history/entity/query_history.dart';
+import '../../../domain/repositories/query_history/query_history_repository.dart';
+import '../../../util/logger.dart';
+import 'collection/query_history.dart';
 
-/// 検索履歴一覧プロバイダー
-final queryHistoriesProviderFamily =
-    FutureProvider.family.autoDispose<List<QueryHistory>, String>((ref, queryString) async {
-  final repository = ref.watch(queryHistoryRepositoryProvider);
-
-  // このStreamは使用しない？
-  // repository.changes(queryString: queryString).listen((queryHistories) {
-  //   ref.state = AsyncValue.data(queryHistories);
-  // });
-  return repository.finds(queryString: queryString);
-});
-
-/// 検索履歴Repositoryプロバイダー
-final queryHistoryRepositoryProvider = Provider<QueryHistoryRepository>(
+/// Isarインスタンスプロバイダー
+///
+/// runApp()前に初期化したIsarインスタンスでoverrideすること
+final isarProvider = Provider<Isar>(
   (ref) => throw UnimplementedError('Provider was not initialized'),
 );
 
-/// 検索履歴Repository
-abstract class QueryHistoryRepository {
-  /// 検索履歴を登録する
-  Future<void> add(QueryHistory queryHistory);
+/// Isar版検索履歴Repositoryプロバイダー
+final isarQueryHistoryRepositoryProvider = Provider<QueryHistoryRepository>(
+  (ref) => IsarQueryHistoryRepository(
+    isar: ref.watch(isarProvider),
+  ),
+);
 
-  /// 検索履歴を削除する
-  Future<void> delete(QueryHistory queryHistory);
+/// Isar版検索履歴Repository
+class IsarQueryHistoryRepository implements QueryHistoryRepository {
+  IsarQueryHistoryRepository({
+    required this.isar,
+  });
 
-  /// 検索履歴一覧を検索する
-  ///
-  /// - 検索条件
-  ///   - 大文字小文字を区別しない [queryString] から始まる
-  /// - ソート
-  ///   - 検索日時の降順
-  /// - 検索件数
-  ///   - 20件
-  Future<List<QueryHistory>> finds({required String queryString});
+  final Isar isar;
 
-  /// 検索履歴一覧のStreamを返す
-  ///
-  /// - 検索条件
-  ///   - 大文字小文字を区別しない [queryString] から始まる
-  /// - ソート
-  ///   - 検索日時の降順
-  /// - 検索件数
-  ///   - 20件
-  Stream<List<QueryHistory>> changes({required String queryString});
+  @override
+  Future<void> add(QueryHistory queryHistory) async {
+    final queryString = queryHistory.queryString.value;
+    logger.v('START TRANSACTION add(): queryString = $queryString');
+    return isar.writeTxn(
+      () async {
+        final id = await isar.queryHistoryCollections.put(
+          QueryHistoryCollection()
+            ..queryString = queryString
+            ..searchedAt = DateTime.now(),
+        );
+        final added = await isar.queryHistoryCollections.get(id);
+        logger.v('END TRANSACTION add(): $added');
+      },
+    );
+  }
+
+  @override
+  Future<void> delete(QueryHistory queryHistory) async {
+    final queryString = queryHistory.queryString.value;
+    logger.v(
+      'START TRANSACTION delete(): queryString = $queryString',
+    );
+    return isar.writeTxn(
+      () async {
+        final count = await isar.queryHistoryCollections.filter().queryStringEqualTo(queryString).deleteAll();
+        logger.v(
+          'END TRANSACTION delete(): queryString = $queryString, '
+          'count = $count',
+        );
+      },
+    );
+  }
+
+  @override
+  Future<List<QueryHistory>> finds({required String queryString}) async {
+    final collections = await _queryBuilder(queryString: queryString).findAll();
+
+    final queries = collections
+        .map(
+          (collection) => QueryHistory.create(collection.queryString),
+        )
+        .toList();
+
+    logger.v(
+      'finds(): queryString = ${queryString.trim()}, '
+      'count = ${queries.length}',
+    );
+    return queries;
+  }
+
+  @override
+  Stream<List<QueryHistory>> changes({required String queryString}) {
+    logger.v('changes(): queryString = ${queryString.trim()}');
+    return _queryBuilder(queryString: queryString).watch().map(
+          (collections) => collections.map((collection) => QueryHistory.create(collection.queryString)).toList(),
+        );
+  }
+
+  /// 検索結果のクエリービルダーを帰す
+  QueryBuilder<QueryHistoryCollection, QueryHistoryCollection, QAfterLimit> _queryBuilder({
+    required String queryString,
+  }) =>
+      isar.queryHistoryCollections
+          .filter()
+          .queryStringStartsWith(queryString.trim(), caseSensitive: false)
+          .sortBySearchedAtDesc()
+          .distinctByQueryString()
+          .limit(20);
 }
