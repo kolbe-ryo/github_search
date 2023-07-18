70c8efc59e6465776bb9aaf1d30bee7abc1dc4c5
kolbe <water.northground@gmail.com>
2023-07-19 00:50:53 +0900

    new avatar page

---
+++ lib/presentation/page/repo/avatar_preview_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class AvatarPreviewPage extends StatelessWidget {
+  const AvatarPreviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
