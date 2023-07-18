1082376270338152a50d65405a1180bc0f6e3b16
kolbe <water.northground@gmail.com>
2023-07-19 00:51:12 +0900

    new repo view page for detail

---
+++ lib/presentation/page/repo/repo_view_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class RepoViewPage extends StatelessWidget {
+  const RepoViewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
