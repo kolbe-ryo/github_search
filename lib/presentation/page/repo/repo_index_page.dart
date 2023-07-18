8d3234ebc82677a8211411a8d5e3bab63c6059ef
kolbe <water.northground@gmail.com>
2023-07-19 00:50:34 +0900

    new repo index page

---
+++ lib/presentation/page/repo/repo_index_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class RepoIndexPage extends StatelessWidget {
+  const RepoIndexPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
